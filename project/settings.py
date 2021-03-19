c1ad52b6a0355b46a25bc9408dc7f6c2c1eaee8a
Michael Yudin <mihelitto@gmail.com>
2021-03-19 19:15:01 +0300

    Add ALOOWED_HOSTS to .env

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ DEBUG = env.bool("DEBUG")
 
 ROOT_URLCONF = "project.urls"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
